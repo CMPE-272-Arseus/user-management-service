fd87f61d00f9e94e893afda1a56dc20fc5164fcf
Gabriel Chen <gabriel.luke.chen@gmail.com>
2021-11-01 16:46:52 -0700

    setup api routes

--- pkg/httpapplication/httphandlers.go
+++ pkg/httpapplication/httphandlers.go
@@ -13,46 +13,73 @@ import (
 // 	}
 // }
 
-// func (s *server) handleErrorEvents(w http.ResponseWriter, error) {
-// }
-
-func (s *server) httpHandlerCoinChange() http.HandlerFunc {
-	// log := s.Logger()
-
+func (s *server) httpHandlerGetUserAccessLevel() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		// d, _ := r.URL.Query()["denominations"]
 
-		// di, err := helpers.Atoiarray(d)
-		// if err != nil {
-		// 	log.Info("httpHandlerCoinChange: invalid denominations in query params", zap.Strings("denominations", d))
-		// 	s.respond(w, nil, http.StatusBadRequest)
-		// 	return
-		// }
-
-		// t, _ := r.URL.Query()["total"]
-		// if len(t) < 1 {
-		// 	log.Info("httpHandlerCoinChange: no total given in query params")
-		// 	s.respond(w, nil, http.StatusBadRequest)
-		// 	return
-		// }
-
-		// ti, err := strconv.Atoi(t[0])
-		// if err != nil {
-		// 	log.Info("httpHandlerCoinChange: invalid total in query params", zap.Strings("total", t))
-		// 	s.respond(w, nil, http.StatusBadRequest)
-		// 	return
-		// }
-
-		// c := solving.NewProblemsCatalogue()
-		// p := c.NewCoinChangeProblem(di, ti)
-
-		// solution, err := p.Solve()
-		// if err != nil {
-		// 	log.Info("httpHandlerCoinChange: solve failed", zap.Error(err))
-		// 	s.respond(w, nil, http.StatusBadRequest)
-		// 	return
-		// }
-
-		// s.respond(w, nil, http.StatusOK)
 	}
 }
+
+func (s *server) httpHandlerPatchUserAccessLevel() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+
+	}
+}
+
+func (s *server) httpHandlerDeleteUserAccessLevel() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+
+	}
+}
+
+func (s *server) httpHandlerPostUserAccessLevel() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+
+	}
+}
+
+func (s *server) httpHandlerListUsersAccessLevels() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+
+	}
+}
+
+// func (s *server) httpHandlerCoinChange() http.HandlerFunc {
+// log := s.Logger()
+
+// return func(w http.ResponseWriter, r *http.Request) {
+// d, _ := r.URL.Query()["denominations"]
+
+// di, err := helpers.Atoiarray(d)
+// if err != nil {
+// 	log.Info("httpHandlerCoinChange: invalid denominations in query params", zap.Strings("denominations", d))
+// 	s.respond(w, nil, http.StatusBadRequest)
+// 	return
+// }
+
+// t, _ := r.URL.Query()["total"]
+// if len(t) < 1 {
+// 	log.Info("httpHandlerCoinChange: no total given in query params")
+// 	s.respond(w, nil, http.StatusBadRequest)
+// 	return
+// }
+
+// ti, err := strconv.Atoi(t[0])
+// if err != nil {
+// 	log.Info("httpHandlerCoinChange: invalid total in query params", zap.Strings("total", t))
+// 	s.respond(w, nil, http.StatusBadRequest)
+// 	return
+// }
+
+// c := solving.NewProblemsCatalogue()
+// p := c.NewCoinChangeProblem(di, ti)
+
+// solution, err := p.Solve()
+// if err != nil {
+// 	log.Info("httpHandlerCoinChange: solve failed", zap.Error(err))
+// 	s.respond(w, nil, http.StatusBadRequest)
+// 	return
+// }
+
+// s.respond(w, nil, http.StatusOK)
+// }
+// }
--- pkg/httpapplication/routes.go
+++ pkg/httpapplication/routes.go
@@ -3,5 +3,10 @@ package httpapplication
 import "net/http"
 
 func (s *server) SetupRoutes() {
-	s.router.HandleFunc("/", s.httpHandlerCoinChange()).Methods(http.MethodGet)
+	s.router.HandleFunc("/{customer_id}/{user_id}", s.httpHandlerGetUserAccessLevel()).Methods(http.MethodGet)
+	s.router.HandleFunc("/{customer_id}/{user_id}", s.httpHandlerPatchUserAccessLevel()).Methods(http.MethodPatch)
+	s.router.HandleFunc("/{customer_id}/{user_id}", s.httpHandlerDeleteUserAccessLevel()).Methods(http.MethodDelete)
+
+	s.router.HandleFunc("/{customer_id}", s.httpHandlerListUsersAccessLevels()).Methods(http.MethodGet)
+	s.router.HandleFunc("/{customer_id}", s.httpHandlerPostUserAccessLevel()).Methods(http.MethodPost)
 }
